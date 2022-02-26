5a50ba06da2880786d2cebc1db3c3cc4d13db953
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-26 11:42:36 +0100

    add users/login endpoint

--- src/server/routers/usersRouter.js
+++ src/server/routers/usersRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { userRegister } = require("../controllers/usersControllers");
+const { userRegister, userLogin } = require("../controllers/usersControllers");
 
 const router = express.Router();
 
 router.post("/register", userRegister);
+router.post("/login", userLogin);
 
 module.exports = router;
