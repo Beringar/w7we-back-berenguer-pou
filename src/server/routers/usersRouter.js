f2f024e75b5a6bea1a52f829bc1797eba8b317cb
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-27 19:33:51 +0100

    added projection to exclude password in getAllUsers controller

--- src/server/routers/usersRouter.js
+++ src/server/routers/usersRouter.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const multer = require("multer");
-const { userRegister, userLogin } = require("../controllers/usersControllers");
+const {
+  userRegister,
+  userLogin,
+  getAllUsers,
+} = require("../controllers/usersControllers");
 
 const router = express.Router();
 
 const upload = multer({ dest: "uploads" });
 
+router.get("/", getAllUsers);
 router.post("/register", upload.single("image"), userRegister);
 router.post("/login", userLogin);
 
