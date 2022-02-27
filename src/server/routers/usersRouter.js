567a8db1e9074790e847099fb78cf0304ffca5e2
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-27 09:23:14 +0100

    added multer / defined uploads folder in server

--- src/server/routers/usersRouter.js
+++ src/server/routers/usersRouter.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const multer = require("multer");
 const { userRegister, userLogin } = require("../controllers/usersControllers");
 
 const router = express.Router();
 
-router.post("/register", userRegister);
+const upload = multer({ dest: "uploads" });
+
+router.post("/register", upload.single("image"), userRegister);
 router.post("/login", userLogin);
 
 module.exports = router;
